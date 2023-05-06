e8c27d7046247066ee6b5991feec1376defa4b67
aalnoman10 <aalnomanm10@gmail.com>
2023-05-06 21:51:03 +0600

    The-china-kitche-api

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const port = 5000;
+const app = express();
+
+const chefs = require('./data/chefs.json');
+
+const cors = require('cors');
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send(chefs)
+})
+
+app.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const selected = chefs.find(chef => chef.id === id)
+    res.send(selected)
+})
+
+app.listen(port, () => {
+    console.log(`server in run port ${port}`);
+})
